beec17f7e8517af7220b373d4a79e522be9f92a3
Lucas Werner <lucas.1werner@gmail.com>
2020-06-25 18:41:53 -0300

    ok

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import SwappingSquaresSpinner from '@bit/bondz.react-epic-spinners.swapping-squares-spinner';
 
 export default function Loading() {
-  return (<SwappingSquaresSpinner color="#000000" size="200" />);
+  return (<div>Loading...</div>);
 }
