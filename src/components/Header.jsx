ff47dc3db0afc3015ed1a6d5b844a754f51f17a6
potew <potew0@gmail.com>
2020-06-25 18:15:50 -0300

    CC 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import profileIcon from "../images/profileIcon.svg";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
 import './Header.css';
 
 const searchBar = (searchTerm, setSearchTerm, radioFilter, setRadioFilter) => {
-  return(
+  return (
     <div>
       <div>
         <input
@@ -14,38 +14,40 @@ const searchBar = (searchTerm, setSearchTerm, radioFilter, setRadioFilter) => {
       </div>
       <div className='radioSearchButtons'>
         <form onChange={(evt) => setRadioFilter(evt.target.value)}>
-          <label for='ingredient'>Ingrediente</label>
-          <input type='radio' name='searchTerm' id='ingredient' value='ingredient' />
-          <label for='name'>Nome</label>
-          <input type='radio' name='searchTerm' id='name' value='name' />
-          <label for='firstLetter'>Primeira Letra</label>
-          <input type='radio' name='searchTerm' id='firstLetter' value='firstLetter' />
+          <label htmlFor="ingredient">Ingrediente</label>
+          <input type="radio" name="searchTerm" id="ingredient" value="ingredient" />
+          <label htmlFor="name">Nome</label>
+          <input type="radio" name="searchTerm" id="name" value="name" />
+          <label htmlFor="firstLetter">Primeira Letra</label>
+          <input type="radio" name="searchTerm" id="firstLetter" value="firstLetter" />
         </form>
       </div>
       <button onClick={() => alert(`Busca por ${searchTerm} e ${radioFilter}`)}>Buscar</button>
     </div>
   );
-}
+};
 
 const Header = (titleTag, isSearchablePage) => {
   const [displaySearch, setDisplaySearch] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
   const [radioFilter, setRadioFilter] = useState('');
 
-  return(
-    <div className='topBar'>
+  return (
+    <div className="topBar">
       <div className="headerBar">
         <Link to="/perfil">
-          <img src={profileIcon} alt='Ícone de perfil'></img>
+          <img src={profileIcon} alt="Ícone de perfil" />
         </Link>
         <h2>{titleTag}</h2>
         { isSearchablePage ? (
-        <button
-          className="searchButton"
-          onClick={() => setDisplaySearch(!displaySearch)}>
-          </button>) : <div></div> }
+          <button
+            className="searchButton"
+            onClick={() => setDisplaySearch(!displaySearch)}
+          />
+        ) : <div /> 
+        }
       </div>
-      <div className='searchBar'> {
+      <div className="searchBar"> {
         displaySearch ?
         searchBar(searchTerm, setSearchTerm, radioFilter, setRadioFilter) :
         null
@@ -53,6 +55,6 @@ const Header = (titleTag, isSearchablePage) => {
       </div>
     </div>
   );
-}
+};
 
 export default Header;
