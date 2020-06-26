bf1deeece976ccfe664fa49be6635a1279f216f2
Lucas Werner <lucas.1werner@gmail.com>
2020-06-26 19:36:15 -0300

    cc

--- src/components/CardFilters.js
+++ src/components/CardFilters.js
@@ -4,18 +4,21 @@ import PropTypes from 'prop-types';
 function CardFilters({ categories, categorySel, setCategorySel }) {
   return (
     <div>
-      <button type="button" value="all" onClick={({ target: { value } }) => setCategorySel(value)}>
+      <button type='button' value='all' onClick={({ target: { value } }) => setCategorySel(value)}>
         All
       </button>
       {categories.slice(0, 5).map(({ category }) => (
         <button
-          type="button"
+          type='button'
           data-testid={`${category}-category-filter`}
           value={category}
           onClick={({ target: { value } }) => {
-            if (categorySel === value) return setCategorySel('all')
-            return setCategorySel(value)}
-          }
+            if (categorySel === value) {
+              return setCategorySel('all');
+            } else {
+              return setCategorySel(value);
+            }
+          }}
         >
           {category}
         </button>
