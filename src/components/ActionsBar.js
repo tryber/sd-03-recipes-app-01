906eb8bee5c61e5b3c9232cd1da0fcb90c655d48
Gustaff <gustavohmfigueiredo@gmail.com>
2020-06-26 18:28:33 -0300

    favorite

--- src/components/ActionsBar.js
+++ src/components/ActionsBar.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import whiteHeart from '../images/whiteHeartIcon.svg';
 import blackHeart from '../images/blackHeartIcon.svg';
+import './ActionsBar.css';
 
 function ActionsBar() {
   const [isFavorite, setIsFavorite] = useState(false);
 
   return (
-    <button onClick={() => setIsFavorite(!isFavorite)}>
+    <button className="hidden-button" onClick={() => setIsFavorite(!isFavorite)}>
       {isFavorite
-        ? <img src={whiteHeart} alt="is not favorite" />
-        : <img src={blackHeart} alt="is not favorite" />
+        ? <img src={blackHeart} alt="is amazing favorite" />
+        : <img src={whiteHeart} alt="is not favorite" />
       }
     </button>
   );
