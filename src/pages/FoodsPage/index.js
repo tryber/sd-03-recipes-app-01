099ae09f65455446e5dcee2d9eef4b2106d2410a
potew <potew0@gmail.com>
2020-06-26 16:50:59 -0300

    Erro CC do nada
    Absolute imports should come before relative imports

--- src/pages/FoodsPage/index.js
+++ src/pages/FoodsPage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
-import Header from '../../components/Header';
 import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
 
 import { Card } from '../../components';
 
