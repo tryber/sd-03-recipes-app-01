89f4ab824f3e0d4d3036238497f8c0e24534cc2c
potew <potew0@gmail.com>
2020-06-26 18:40:52 -0300

    CC²²²

--- src/pages/ExploreFoodIngredientsPage/index.js
+++ src/pages/ExploreFoodIngredientsPage/index.js
@@ -5,7 +5,7 @@ import { Footer } from '../../components';
 function ExploreFoodIngredientsPage() {
   return (
     <div>
-      <Header titleTag='Explorar Ingredientes' isSearchablePage={false} />
+      <Header titleTag="Explorar Ingredientes" isSearchablePage={false} />
       <h1>This is ExploreFoodIngredientsPage</h1>
       <Footer />
     </div>
