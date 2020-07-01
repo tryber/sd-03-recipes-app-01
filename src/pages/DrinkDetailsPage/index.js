211cb2b8f0b65dfb1c2ab022b97f5f0ca2939168
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-01 18:30:14 -0300

    changin initial state to nul

--- src/pages/DrinkDetailsPage/index.js
+++ src/pages/DrinkDetailsPage/index.js
@@ -8,14 +8,13 @@ import { fetchFoodsApi, handleFoodsData } from '../../services/APIs/FOODS_API';
 import useRequisition from '../../hooks/requisition';
 
 function DrinkDetailsPage({ id }) {
-  const [drink, setDrink] = useState({});
+  const [drink, setDrink] = useState(null);
   const fetchDrink = useCallback(() => fetchDrinkApi(`lookup.php?i=${id}`)
     .then(({ drinks }) => setDrink(handleDrinksData(drinks[0])))
   , [setDrink, id]);
-
   const [{ loading, error }] = useRequisition(fetchDrink);
 
-  const [recomends, setRecomends] = useState({});
+  const [recomends, setRecomends] = useState(null);
   const fetchRecomends = () => fetchFoodsApi()
     .then(({ meals }) => setRecomends(meals.slice(0, 6).map((meal) => handleFoodsData(meal))));
   const [{ loadingRecom, errorRecom }] = useRequisition(fetchRecomends);
@@ -26,7 +25,7 @@ function DrinkDetailsPage({ id }) {
     <div>
       <DetailsCard type="drink" eat={drink} />
       {errorRecom && <h3 data-testid="error-details">Aconteceu algo errado em recomendações</h3>}
-      {!errorRecom && loadingRecom && <h3>Carrgando detalhes de comida...</h3>}
+      {!errorRecom && loadingRecom && <h3>Carregando detalhes de comida...</h3>}
       {!errorRecom && !loadingRecom && recomends && <Carrosel cards={recomends} />}
     </div>
   );
