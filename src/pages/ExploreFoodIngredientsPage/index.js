ee7c238d748209d25576bac77913e2cbf6e59aa9
Lucas Werner <lucas.1werner@gmail.com>
2020-07-06 22:20:20 -0300

    CC

--- src/pages/ExploreFoodIngredientsPage/index.js
+++ src/pages/ExploreFoodIngredientsPage/index.js
@@ -29,7 +29,7 @@ function ExploreFoodIngredientsPage() {
         setLoading(false);
       })
       .catch((err) => {
-        console.log(err);     
+        console.log(err);    
       });
   }, [setLoading]);
 
