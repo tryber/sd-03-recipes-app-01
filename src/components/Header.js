cd387eece2daf1acac7d37e5ae23ff3279fd74a4
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-07-08 13:54:49 -0300

    Css

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = ({ titleTag, filterMode }) => {
         ) : <div />
         }
       </div>
-      <div className="searchBar"> {
+      <div className="searchBar newFormCss"> {
         displaySearch ?
           <SearchBar
             setFilter={filterMode}
